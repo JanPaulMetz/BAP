5d74ac5ad9444f8b58400af026aa19731636ea80
JanPaul <jpmetz1@hotmail.com>
2023-04-26 10:58:35 +0200

    Added polynomial model
    Added data extraction

--- src/polynomial_model.py
+++ src/polynomial_model.py
@@ -1,1 +1,16 @@
+from random_plant import *
 
+#------------  Generate data ------------#
+t = np.linspace(0,100,500)  # 100 sec
+F = 100                     # 100 Hz
+SAMPLE_RATE = 1_000         # 1kHz sample rate
+DURATION = 10               # 10 seconds
+
+# Generate tf
+random_tf_discrete = generate_discrete_tf(SAMPLE_RATE)
+
+# Generate discrete sine
+time_axis, signal_t = generate_discrete_sine(F, SAMPLE_RATE, DURATION)
+
+# Simulate tf using signal_t
+t_simulate = np.linspace(0,duration, len(signal_t))
