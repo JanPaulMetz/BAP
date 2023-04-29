c42d28e186c8a9c9d3445095b15bfac63c7dc4c2
JanPaul <jpmetz1@hotmail.com>
2023-04-29 16:58:01 +0200

    new function "generate_data()"
    Also added linear regression

--- src/polynomial_model.py
+++ src/polynomial_model.py
@@ -1,38 +1,49 @@
-from random_plant import *
+""" Data Processing """
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+from random_plant import *
+
 
 #------------ Generate data ------------#
-F = 1                   # 100 Hz
-SAMPLE_RATE = 1_000         # 1kHz sample rate
-DURATION = 1               # 10 seconds
-t = np.linspace(0,DURATION,100)  # sec 
+F = 10                              # Hz
+SAMPLE_RATE = 100_000               # 1kHz sample rate
+DURATION = 5                        # 10 seconds
+N_SAMPLES = DURATION*SAMPLE_RATE    # Number of samples
+# t = np.arange(DURATION,step=DURATION/N_SAMPLES)
 
-# Generate tf
-random_tf_discrete = generate_discrete_tf(SAMPLE_RATE)
-print("TF : ", random_tf_discrete)
-# Generate discrete sine
-time_axis, signal_t = generate_discrete_sine(F, SAMPLE_RATE, DURATION)
+data_in, t_out, data_out = generate_data(F, SAMPLE_RATE, DURATION)
 
-# Simulate tf using signal_t
-t_simulate = np.linspace(0,DURATION, len(signal_t))
-t_sim, y_sim = signal.dlsim(system = random_tf_discrete,u = signal_t, t = time_axis)
-print("t sim: ", t_sim )
 #------------ Preprocess data ------------#
-sim_fft = calculate_signal_fft(t_sim, y_sim, SAMPLE_RATE)
-print(sim_fft[0][1:100])
+fft_in_x, fft_in_y = calculate_signal_fft(t_out, data_in, SAMPLE_RATE)
+fft_out_x, fft_out_y = calculate_signal_fft(t_out, data_out, SAMPLE_RATE) # signal_t
+fft_out_psd = np.abs(fft_out_y)/(N_SAMPLES/2) # power spectral density
 
-# sim_fft = [sim_fft[0][0:int(len(sim_fft[0])/2)], sim_fft[0][-int(len(sim_fft[0])/2):0]]
-# plt.figure(1)
-# plt.plot(time_axis, signal_t)
+INDEX_MID = int(SAMPLE_RATE/2)
+INDEX_END = int(SAMPLE_RATE)
+fft_out_x = np.append(0,[fft_out_x[INDEX_MID:INDEX_END - 1],fft_out_x[0:INDEX_MID - 1]])
+fft_out_psd = np.append(0,[fft_out_psd[INDEX_MID:INDEX_END - 1],fft_out_psd[0:INDEX_MID - 1]])
+
+#--- test ----#
+fft_in_y = np.array(np.abs(fft_in_y))
+fft_out_y = np.array(np.abs(fft_out_y))
+x = fft_in_y[:,np.newaxis]
+y = fft_out_y[:,np.newaxis]
+print(np.shape(x))
+#------------ Define model ------------#
+linear_model = LinearRegression()
+
+#------------ Train model ------------#
+linear_model.fit(x, y)
+score = linear_model.score(x, y)
+
+print(score)
+print(linear_model.coef_)
+
+# plt.figure()
+# plt.plot(t,filt, color='r')
+# plt.plot(t, signal_t, color= 'b')
+
+# plt.figure()
+# plt.stem(fft_out_x[:], fft_out_psd[:], 'x')
 # plt.show()
-
-plt.figure(2)
-plt.plot(t_sim, y_sim, '+')
-plt.xlabel("time")
-plt.show()
-
-plt.figure(3)
-plt.plot(sim_fft[0], abs(sim_fft[1]), '-')
-plt.xlabel("omega")
-plt.ylabel("amplitude")
-plt.show()
