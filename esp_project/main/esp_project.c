be3bedf52708fd7a52e8c7c8fd0b4e9c90f081ef
JanPaul <jpmetz1@hotmail.com>
2023-05-08 09:23:10 +0200

    Setup esp32 project
    Note: Always add a vscode configuration, since it is in gitignore

---
+++ esp_project/main/esp_project.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "sdkconfig.h"
+#include "driver/gpio.h" 
+
+void init(void)
+{
+    // Initialize timer 
+    // timer_init(GROUP_NUM);
+}
+
+void app_main(void)
+{
+    init();
+    while(1)
+    {
+        
+    }
+}
