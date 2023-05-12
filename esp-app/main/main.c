150fa95594387ac56964bdf2e63ffc0ce4038659
Joost Jaspers <joostjaspers02@gmail.com>
2023-05-12 16:18:48 +0200

    Serial echo
    Werkende echo van serieel signaal

--- esp-app/main/main.c
+++ esp-app/main/main.c
@@ -9,43 +9,50 @@
 #include <stdio.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include "esp_log.h"
 #include "string.h"
 #include "driver/uart.h"
 #include "driver/gpio.h"
 #include "wavelength_control.h"
+#include "sdkconfig.h"
+
+const uart_port_t uart_num = UART_NUM_0;
 
 static void init_led(void)
 {
     gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT); //Set the pin direction of the led(pin2) to out. 
-    //gpio_set_level(GPIO_NUM_2, 1); //Put the blue LED(pin2) on. 
+    gpio_set_level(GPIO_NUM_2, 0); //Put the blue LED(pin2) off. 
 }
 
 static void init_uart(void)
 {
-   const uart_port_t uart_num = UART_NUM_0;
-    // uart_config_t uart_config = {
-    //     .baud_rate = 115200,
-    //     .data_bits = UART_DATA_8_BITS,
-    //     .parity = UART_PARITY_DISABLE,
-    //     .stop_bits = UART_STOP_BITS_1,
-    //     .flow_ctrl = UART_HW_FLOWCTRL_CTS_RTS,
-    //     .rx_flow_ctrl_thresh = 122
-    // };
+    
+   
+   // Setup UART buffered IO with event queue
+    const int uart_buffer_size = (1024*2);
+    QueueHandle_t uart_queue;
+    // Install UART driver using an event queue here
+    ESP_ERROR_CHECK(uart_driver_install(uart_num, uart_buffer_size, \
+                                        uart_buffer_size, 10, &uart_queue, 0));
+    
     // Configure UART parameters
-    //ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
     ESP_ERROR_CHECK(uart_set_baudrate(uart_num, 115200));
     ESP_ERROR_CHECK(uart_set_word_length(uart_num, UART_DATA_8_BITS));
     ESP_ERROR_CHECK(uart_set_parity(uart_num,UART_PARITY_DISABLE));
     ESP_ERROR_CHECK(uart_set_stop_bits(uart_num, UART_STOP_BITS_1));
-    ESP_ERROR_CHECK(uart_set_hw_flow_ctrl(uart_num, UART_HW_FLOWCTRL_CTS_RTS,122));
-   // gpio_set_level(GPIO_NUM_2, 1); //Put the blue LED(pin2) on
-    //ESP_ERROR_CHECK(uart_set_mode(uart_num, UART_MODE_UART));
-    vTaskDelay(2000/portTICK_PERIOD_MS);
+    ESP_ERROR_CHECK(uart_set_hw_flow_ctrl(uart_num, UART_HW_FLOWCTRL_DISABLE,122));
+    ESP_ERROR_CHECK(uart_set_mode(uart_num, UART_MODE_UART));
 
+    ESP_ERROR_CHECK(uart_set_pin(UART_NUM_0, 1, 3, 18, 19));
+
+
+
+    //Blink Led
+    vTaskDelay(2000/portTICK_PERIOD_MS);
     gpio_set_level(GPIO_NUM_2, 0); //Put the blue LED off.
     //Configure UART parameters
-    vTaskDelay(3000/portTICK_PERIOD_MS);
-    gpio_set_level(GPIO_NUM_2, 1); //Put the blue LED off.
+    vTaskDelay(2000/portTICK_PERIOD_MS);
+    gpio_set_level(GPIO_NUM_2, 0); //Put the blue LED off.
 
 }
 
@@ -55,12 +62,24 @@ int app_main(void)
     //Initialize:
     init_led();
     init_uart();
-    while(1){
-         gpio_set_level(GPIO_NUM_2, 0); //Put the blue LED off.
-        // break;
-        vTaskDelay(1000/portTICK_PERIOD_MS);
-        gpio_set_level(GPIO_NUM_2, 1); //Put the blue LED on.
-        vTaskDelay(1000/portTICK_PERIOD_MS);
+    //char* message = "Nog geen echo";
+    
+    while (1)
+    {
+        //gpio_set_level(GPIO_NUM_2, 0); //Put the blue LED off.
+        vTaskDelay(500/portTICK_PERIOD_MS);
+        
+        // Read data from UART.
+        uint8_t data[128];
+        int length = 0;
+        ESP_ERROR_CHECK(uart_get_buffered_data_len(uart_num, (size_t*)&length));
+        length = uart_read_bytes(uart_num, data, length, 100);
+        if(length>101){
+            gpio_set_level(GPIO_NUM_2, 1); //Put the blue LED on.
+        }
+        //Write back data to UART.
+        uart_write_bytes(uart_num, (const char*)data, length);
+        vTaskDelay(500/portTICK_PERIOD_MS);
     }
    
     //init_uart();
--- scripts/serial_read.py
+++ scripts/serial_read.py
@@ -1,11 +1,19 @@
 import time 
-from serial import Serial
+import serial
 
-ser = Serial(port="COM7", baudrate=115200)
+ser = serial.Serial(port="COM5",
+            baudrate=115200,
+            bytesize=serial.EIGHTBITS,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE)
 
 # ser.open()
-
+# b = bytes()
+MESSAGE = 'Boodschap!'
 while 1:
-    print(ser.read())
-    time.sleep(1)
-
+    ser.write((MESSAGE + '\n').encode())
+    time.sleep(0.1)
+    READ_LINE = ser.readline()
+    SERIAL_MESSAGE = READ_LINE.decode().replace('\n','')
+    print(SERIAL_MESSAGE)
+    time.sleep(0.01)
