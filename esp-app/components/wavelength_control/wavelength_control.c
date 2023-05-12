46090c27af0b1cfc0bf715a948916a181983b6cc
Joost Jaspers <joostjaspers02@gmail.com>
2023-05-12 09:48:59 +0200

    Werkende knipperende LED
    Op laptop Joost

--- esp-app/components/wavelength_control/wavelength_control.c
+++ esp-app/components/wavelength_control/wavelength_control.c
@@ -7,34 +7,35 @@ void init(){
 
 float update_control_signal(float setpoint, float interfero_dc, float period)
 {
-    float error, p_term, i_term, control_signal, integral;
+    // float error, p_term, i_term, control_signal, integral;
 
-    // Proportional and Integral gains:
-    const float k_p = 1.0;
-    const float k_i = 0.0;
-    const float maxintegral = 300;
-    const float minintegral = -300;
+    // // Proportional and Integral gains:
+    // const float k_p = 1.0;
+    // const float k_i = 0.0;
+    // const float maxintegral = 300;
+    // const float minintegral = -300;
 
-    // Calculate error:
-    error = setpoint - interfero_dc;
+    // // Calculate error:
+    // error = setpoint - interfero_dc;
 
-    //Calculate integral:
-    integral += (error*period); // Moet hier niet nog een tijd iets bij?
-    // Prevent winding due to integral term: 
-    if(integral>maxintegral){
-        integral = maxintegral;
-    }
-    else if (integral<minintegral)
-    {
-        integral = minintegral;
-    }
+    // //Calculate integral:
+    // integral += (error*period); // Moet hier niet nog een tijd iets bij?
+    // // Prevent winding due to integral term: 
+    // if(integral>maxintegral){
+    //     integral = maxintegral;
+    // }
+    // else if (integral<minintegral)
+    // {
+    //     integral = minintegral;
+    // }
 
-    // Calculate PI terms:
-    p_term = k_p*error;
-    i_term = k_i*integral; 
+    // // Calculate PI terms:
+    // p_term = k_p*error;
+    // i_term = k_i*integral; 
 
-    // Calculate new control_signal:
-    control_signal = p_term + i_term;
+    // // Calculate new control_signal:
+    // control_signal = p_term + i_term;
 
-    return control_signal; 
+    // return control_signal; 
+    return 1;
 }
--- esp-app/main/main.c
+++ esp-app/main/main.c
@@ -14,42 +14,46 @@
 #include "driver/gpio.h"
 #include "wavelength_control.h"
 
+void init(void)
+{
+    gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT); //Set the pin direction of the led to out. 
+    gpio_set_level(GPIO_NUM_2, 1); //Put the blue LED on. 
+}
+
+
 
 void app_main(void)
 {
     /* Configure parameters of an UART driver,
      * communication pins and install the driver */
-    uart_config_t uart_config = {
-        .baud_rate = ECHO_UART_BAUD_RATE,
-        .data_bits = UART_DATA_8_BITS,
-        .parity    = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-        .source_clk = UART_SCLK_DEFAULT,
-    };
-    int intr_alloc_flags = 0;
 
-    uart_config_t uart_config = {
-        .baud_rate = 115200,
-        .data_bits = UART_DATA_8_BITS,
-        .parity = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_CTS_RTS,
-        .rx_flow_ctrl_thresh = 122,
-    };
+
+    // uart_config_t uart_config = {
+    //     .baud_rate = 115200,
+    //     .data_bits = UART_DATA_8_BITS,
+    //     .parity = UART_PARITY_DISABLE,
+    //     .stop_bits = UART_STOP_BITS_1,
+    //     .flow_ctrl = UART_HW_FLOWCTRL_CTS_RTS,
+    //     .rx_flow_ctrl_thresh = 122,
+    // };
     // Configure UART parameters
-    ESP_ERROR_CHECK(uart_param_config(UART_NUM_0, &uart_config));
+    // ESP_ERROR_CHECK(uart_param_config(UART_NUM_0, &uart_config));
 
+    // init pin 2 
+    
+    init();
     while(1)
     {
-        // Write data to UART.
-        char* test_str = "H\n";
-        uart_write_bytes(UART_NUM_0, (const char*)test_str, strlen(test_str));
+        gpio_set_level(GPIO_NUM_2, 0); //Put the blue LED on.
+        // break;
         vTaskDelay(1000/portTICK_PERIOD_MS);
+        gpio_set_level(GPIO_NUM_2, 1); //Put the blue LED on.
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+        // Write data to UART.
+        // char* test_str = "H\n";
+        // uart_write_bytes(UART_NUM_0, (const char*)test_str, strlen(test_str));
+        // vTaskDelay(1000/portTICK_PERIOD_MS);
+        // gpio_set_level(GPIO_NUM_2, 0);
+        // vTaskDelay(1000/portTICK_PERIOD_MS);
     }
-}
-
-void app_main(void)
-{
-    xTaskCreate(echo_task, "uart_echo_task", ECHO_TASK_STACK_SIZE, NULL, 10, NULL);
 }
