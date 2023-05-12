b8bdc438943179c405cd96077cd050854a545ca1
Joost Jaspers <joostjaspers02@gmail.com>
2023-05-12 10:50:27 +0200

    UART DOET HET NIET

--- esp-app/main/main.c
+++ esp-app/main/main.c
@@ -14,46 +14,70 @@
 #include "driver/gpio.h"
 #include "wavelength_control.h"
 
-void init(void)
+static void init_led(void)
 {
-    gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT); //Set the pin direction of the led to out. 
-    gpio_set_level(GPIO_NUM_2, 1); //Put the blue LED on. 
+    gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT); //Set the pin direction of the led(pin2) to out. 
+    //gpio_set_level(GPIO_NUM_2, 1); //Put the blue LED(pin2) on. 
 }
 
-
-
-void app_main(void)
+static void init_uart(void)
 {
-    /* Configure parameters of an UART driver,
-     * communication pins and install the driver */
-
-
+   const uart_port_t uart_num = UART_NUM_0;
     // uart_config_t uart_config = {
     //     .baud_rate = 115200,
     //     .data_bits = UART_DATA_8_BITS,
     //     .parity = UART_PARITY_DISABLE,
     //     .stop_bits = UART_STOP_BITS_1,
     //     .flow_ctrl = UART_HW_FLOWCTRL_CTS_RTS,
-    //     .rx_flow_ctrl_thresh = 122,
+    //     .rx_flow_ctrl_thresh = 122
     // };
     // Configure UART parameters
-    // ESP_ERROR_CHECK(uart_param_config(UART_NUM_0, &uart_config));
+    //ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
+    ESP_ERROR_CHECK(uart_set_baudrate(uart_num, 115200));
+    ESP_ERROR_CHECK(uart_set_word_length(uart_num, UART_DATA_8_BITS));
+    ESP_ERROR_CHECK(uart_set_parity(uart_num,UART_PARITY_DISABLE));
+    ESP_ERROR_CHECK(uart_set_stop_bits(uart_num, UART_STOP_BITS_1));
+    ESP_ERROR_CHECK(uart_set_hw_flow_ctrl(uart_num, UART_HW_FLOWCTRL_CTS_RTS,122));
+   // gpio_set_level(GPIO_NUM_2, 1); //Put the blue LED(pin2) on
+    //ESP_ERROR_CHECK(uart_set_mode(uart_num, UART_MODE_UART));
+    vTaskDelay(2000/portTICK_PERIOD_MS);
 
-    // init pin 2 
-    
-    init();
-    while(1)
-    {
-        gpio_set_level(GPIO_NUM_2, 0); //Put the blue LED on.
+    gpio_set_level(GPIO_NUM_2, 0); //Put the blue LED off.
+    //Configure UART parameters
+    vTaskDelay(3000/portTICK_PERIOD_MS);
+    gpio_set_level(GPIO_NUM_2, 1); //Put the blue LED off.
+
+}
+
+
+int app_main(void)
+{
+    //Initialize:
+    init_led();
+    init_uart();
+    while(1){
+         gpio_set_level(GPIO_NUM_2, 0); //Put the blue LED off.
         // break;
         vTaskDelay(1000/portTICK_PERIOD_MS);
         gpio_set_level(GPIO_NUM_2, 1); //Put the blue LED on.
         vTaskDelay(1000/portTICK_PERIOD_MS);
-        // Write data to UART.
-        // char* test_str = "H\n";
-        // uart_write_bytes(UART_NUM_0, (const char*)test_str, strlen(test_str));
-        // vTaskDelay(1000/portTICK_PERIOD_MS);
-        // gpio_set_level(GPIO_NUM_2, 0);
-        // vTaskDelay(1000/portTICK_PERIOD_MS);
     }
+   
+    //init_uart();
+    //Loop:
+    // while(1)
+    // {
+    //     gpio_set_level(GPIO_NUM_2, 0); //Put the blue LED off.
+    //     // break;
+    //     vTaskDelay(1000/portTICK_PERIOD_MS);
+    //     gpio_set_level(GPIO_NUM_2, 1); //Put the blue LED on.
+    //     vTaskDelay(1000/portTICK_PERIOD_MS);
+    //     // Write data to UART.
+    //     // char* test_str = "H\n";
+    //     // uart_write_bytes(UART_NUM_0, (const char*)test_str, strlen(test_str));
+    //     // vTaskDelay(1000/portTICK_PERIOD_MS);
+    //     // gpio_set_level(GPIO_NUM_2, 0);
+    //     // vTaskDelay(1000/portTICK_PERIOD_MS);
+    // }
+    return 0;
 }
